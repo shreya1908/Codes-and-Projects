1dbd9e2c154d02f68937154ea94396542eeed1d0
SHREYA BASU <73105906+shreya1908@users.noreply.github.com>
2021-08-08 20:15:32 +0530

    Created Product.cc

---
+++ Product.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+	cout << "Enter two numbers: " << endl;
+	double a, b ;
+	cin >> a;
+	cin >> b;
+	cout << "Product: " << (a*b) << endl;
+    return 0;
+}
